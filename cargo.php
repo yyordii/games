d88cc2ec6606b73997739750852b862fc1cc69c4
yyordii <yordivandenmeijdenberg@gmail.com>
2023-10-09 16:10:04 +0200

    updated
    updated cargo and fighter

--- cargo.php
+++ cargo.php
@@ -4,7 +4,13 @@ namespace Space;
 include_once 'spaceship.php';
 
 class cargoship extends \Spaceship{
-
+    public function __construct(
+        $storage = 100,
+        $fuel = 100,
+        $hitPoints = 500
+    ) {
+        parent::__construct($storage, $fuel, $hitPoints);
+    }
 }
 
 ?>
--- fighter.php
+++ fighter.php
@@ -4,6 +4,15 @@ namespace Space;
 include_once 'spaceship.php';
 
 class fighterjett extends \Spaceship{
+    public int $speed;
+    public function __construct(
+        $ammo = 100,
+        $fuel = 100,
+        $hitPoints = 75,
+        $speed = 100
+    ) {
+        parent::__construct($ammo, $fuel, $hitPoints);
+    }
     
 }
 
